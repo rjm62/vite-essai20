bb11048463114ae42a74cdb852baa3570a9f52db
rjm <jeanmichelrochoy62@gmail.com>
2024-06-27 17:50:42 +0200

    essai 2

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2> formulaire </h2>
+      <h2> contact </h2>
       <Form />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
