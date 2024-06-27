69fa56cb8f9954a5d8406d6983a0584992378e11
rjm <jeanmichelrochoy62@gmail.com>
2024-06-27 15:12:27 +0200

    deploiement formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Form from './Form'
+import {Form} from './Form'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
